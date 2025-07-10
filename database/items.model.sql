648fc35e0e7c5f7e385e31717743b2847add5762
pingweiii <pwchen31@gmail.com>
2025-07-10 19:27:08 +0800

    feat(items table): added another column for the status of the item

--- database/items.model.sql
+++ database/items.model.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS items (
-    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     item_name VARCHAR(150) NOT NULL,
     description TEXT,
     quantity INTEGER DEFAULT 0,
     price NUMERIC(10, 2) NOT NULL,
     category VARCHAR(100),
+    status VARCHAR(50) DEFAULT 'available',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
