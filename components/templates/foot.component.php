73fb55230bf88bc761e75134f4cece72a99ccd38
sijeeee <ceejjay73@gmail.com>
2025-07-10 22:16:40 +0800

    feat (footer): added base layout container

---
+++ components/templates/foot.component.php
@@ -0,0 +1,11 @@
+<?php
+function footer() {
+    ?>
+    <footer class="mineforge-footer">
+        <div class="footer-grid">
+            <!-- Columns will be added in future commits -->
+        </div>
+    </footer>
+    <?php
+}
+?>
