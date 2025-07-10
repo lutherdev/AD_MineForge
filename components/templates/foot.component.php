0fa45a5142712a2d4729c7937e4169c6cb033ddb
sijeeee <ceejjay73@gmail.com>
2025-07-10 20:54:49 +0800

    feat (footer): added about us column

--- components/templates/foot.component.php
+++ components/templates/foot.component.php
@@ -3,7 +3,13 @@ function footer() {
     ?>
     <footer class="mineforge-footer">
         <div class="footer-grid">
-            <!-- Columns will be added in future commits -->
+        <div class="footer-column">
+            <h3>ABOUT THE GUILD</h3>
+            <ul>
+                <li><a href="about-us">Our Legacy</a></li>
+                <li><a href="about-us">Guild Masters</a></li>
+            </ul>
+        </div>
         </div>
     </footer>
     <?php
