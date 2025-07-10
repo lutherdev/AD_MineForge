0a12c5e43d9b4c8eaa683ee05fe034f8f7b4cdae
pingweiii <pwchen31@gmail.com>
2025-07-10 20:39:24 +0800

    feat(login page): added another div for the login container

--- pages/login/index.php
+++ pages/login/index.php
@@ -1,14 +1,25 @@
-<form action="/handlers/auth.handler.php" method="POST" class="login-form">
-  <div class="form-group">
-    <label>Username: </label>
-    <input type="text" name="username" required>
-  </div>
+<?php
+$error = $_GET['error'] ?? '';
+?>
 
-  <div class="form-group">
-    <label>Password: </label>
-    <input type="password" id="password" name="password" class="form-input" required>
-  </div>
+<div class="login-container">
+  <div class="parchment-login">
+    <h2 class="login-title">MineForge Access</h2>
+    <div class="login-divider">⚒⚒⚒</div>
 
-  <input type="hidden" name="action" value="login">
-  <button type="submit" class="login-btn">Enter The Mines</button>
-</form>
+    <form action="/handlers/auth.handler.php" method="POST" class="login-form">
+      <div class="form-group">
+        <label>Username: </label>
+        <input type="text" name="username" required>
+      </div>
+
+      <div class="form-group">
+        <label>Password: </label>
+        <input type="password" id="password" name="password" class="form-input" required>
+      </div>
+
+      <input type="hidden" name="action" value="login">
+      <button type="submit" class="login-btn">Enter The Mines</button>
+    </form>
+  </div>
+</div>
