f9e6ab49007d959093172ef9dac2272534860d71
sijeeee <ceejjay73@gmail.com>
2025-07-10 18:44:53 +0800

    feat (navbar): basic structure navbar

---
+++ components/templates/nav.component.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+    <div class="navbar-container">
+        <div class="navbar-logo">
+            <a href="homepage">
+                <img src="<?php echo htmlspecialchars($logo); ?>"
+                     alt="<?php echo htmlspecialchars($alt_logo); ?>"
+                     class="logo-img">
+                <span class="logo-text">MineForge</span>
+            </a>
+        </div>
+    </div>
+</nav>
