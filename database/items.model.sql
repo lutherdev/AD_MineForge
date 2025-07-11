8288636cda83ec6f6f4a12431850180b5c1edc0f
Pinggg <pwchen31@gmail.com>
2025-07-11 23:14:47 +0800

    feat(items.model.sql): added column for image and added not null in other columns

--- database/items.model.sql
+++ database/items.model.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS items (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     item_name VARCHAR(150) NOT NULL,
-    description TEXT,
+    description TEXT NOT NULL,
     quantity INTEGER DEFAULT 0,
     price NUMERIC(10, 2) NOT NULL,
-    category VARCHAR(100),
-    status VARCHAR(50) DEFAULT 'available',
+    category VARCHAR(100) NOT NULL,
+    img_path VARCHAR(100) NOT NULL,
+    status VARCHAR(50) DEFAULT 'is-active',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
