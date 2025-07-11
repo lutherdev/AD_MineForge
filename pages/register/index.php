fbdeb870886be5f364cf8d0b96d3776dd8c2b450
pingweiii <pwchen31@gmail.com>
2025-07-11 13:31:03 +0800

    feat(register.php): add username and password input boxes

--- pages/register/index.php
+++ pages/register/index.php
@@ -6,3 +6,14 @@
     </form>
   </div>
 </div>
+
+
+<div class="input-group">
+  <label>Username: </label>
+  <input type="text" name="username" required>
+</div>
+
+<div class="input-group">
+  <label>Password: </label>
+  <input type="password" name="password" required>
+</div>
