0dd868ada96faf0072152e751df942b5adcd3368
pingweiii <pwchen31@gmail.com>
2025-07-11 14:39:29 +0800

    feat(contact-us): added base layout for contact-us page

---
+++ pages/contact-us/index.php
@@ -0,0 +1,13 @@
+<main class="contactus-container">
+    <div class="location">
+        <h1>Location of the Minekeeper</h1>
+        <div class="medieval-map">
+            <img src="/pages/contact-us/assets/img/mappp.png" alt="Mine Location Map">
+        </div>
+        <div class="contact-info">
+            <p><span class="label">Coordinates: </span>333 Sampaloc, Kingdom of Manila</p>
+            <p><span class="label">Whispering Horn: </span>+63 987 654 3210</p>
+            <p><span class="label">Pigeon Scroll: </span>mineforge333@gmail.com</p>
+        </div>
+    </div>
+</main>
