207e7bdbe7b64a7c37d618ae80772fc6b2daa311
pingweiii <pwchen31@gmail.com>
2025-07-11 13:30:27 +0800

    feat(register.php): register form layout with heading and form tag

---
+++ pages/register/index.php
@@ -0,0 +1,8 @@
+<div class="register-container">
+  <div class="register-box">
+    <h2>Register</h2>
+    <form action="handlers/auth.handler.php" method="POST">
+      <!-- Inputs to be added later -->
+    </form>
+  </div>
+</div>
