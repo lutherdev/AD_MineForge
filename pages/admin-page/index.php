e92d45ce4b2b6fe068e3b6c46538610fe5e6ab03
sijeeee <ceejjay73@gmail.com>
2025-07-11 22:20:47 +0800

    feat (admin-page): added layout and an admin card

---
+++ pages/admin-page/index.php
@@ -0,0 +1,14 @@
+<?php
+require_once BASE_PATH . '/bootstrap.php';
+?>
+<link rel="stylesheet" href="/pages/admin-page/assets/css/admin-page.css">
+<main class="admin-page">
+    <div class="admin-container">
+        <div class="admin-grid">
+            <div class="admin-card" onclick="location.href='create.php'">
+                <h3>Create Item</h3>
+                <div class="admin-icon">⛏</div>
+            </div>
+        </div>
+    </div>
+</main>
