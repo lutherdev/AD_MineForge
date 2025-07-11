0ec59e00f6286b3921a979ffbb2b594abd89c157
pingweiii <pwchen31@gmail.com>
2025-07-11 13:32:01 +0800

    feat(register): add role select dropdown and wallet

--- pages/register/index.php
+++ pages/register/index.php
@@ -28,3 +28,18 @@
   <label>Lastname: </label>
   <input type="text" name="lastname" required>
 </div>
+
+
+<div class="input-group">
+  <label>Role: </label>
+  <select name="role" required>
+    <option value="">--Select Role--</option>
+    <option value="customer">Customer</option>
+    <option value="admin">Admin</option>
+  </select>
+</div>
+
+<div class="input-group">
+  <label>Wallet: </label>
+  <input type="text" name="wallet" required>
+</div>
